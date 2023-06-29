b1c11a1159a53d8ca12dc075d9ec980cf56f5338
SOS-Tecnologia-Alunos <>
2023-06-29 12:03:26 -0300

    configurações iniciais
    configurações inicia
    criação de pastas
    teste do servidor local de dev

---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+function main_page(){
+    return(
+        <div>
+        <n1>clarissa monteiro</n1>
+        <h2>iniciando o portifolio</h2>
+        <ol>o que vamos fazer com o portifolio
+            <li>aprender a usae o git</li>
+            <li>aprender a crias um projeto em react</li>
+            <li>criar uma pagina bilingue</li>
+            <li>criar uma pagina responsiva</li>
+            <li>fazer uma estilazação profissional com css</li>
+        </ol>
+        </div>
+    )
+}
+export default main_page
