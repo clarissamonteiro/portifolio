08c39f7d12b9b90c8ffb7001b33973bcee15688f
SOS-Tecnologia-Alunos <>
2023-07-06 12:21:34 -0300

    configuração de estilos
    configuração do globals.css e tags de portifolio

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,11 @@
+import Portifolio from './portifolio.jsx'
 function main_page(){
     return(
-        <div>
-        <n1>clarissa monteiro</n1>
-        <h2>iniciando o portifolio</h2>
-        <ol>o que vamos fazer com o portifolio
-            <li>aprender a usae o git</li>
-            <li>aprender a crias um projeto em react</li>
-            <li>criar uma pagina bilingue</li>
-            <li>criar uma pagina responsiva</li>
-            <li>fazer uma estilazação profissional com css</li>
-        </ol>
-        </div>
+        <div >
+
+   <title>clarissa monteiro</title>
+   <Portifolio></Portifolio>
+   </div>
     )
 }
 export default main_page
---
+++ pages/portifolio.jsx
@@ -0,0 +1,16 @@
+function Portifolio(){
+    return(
+        <div>
+            <h1>portifolio</h1>
+   <nav>
+    <ul className="navbar">
+        <li id="sobre">sobre mim</li>
+        <li id="projetos">projetos</li>
+        <li id="carreira">carreira</li>
+        <li id="contato">contato</li>
+    </ul>
+   </nav>
+        </div>
+    )
+}
+export default Portifolio 
